02c6b4515a8b2886ba9d3896edafaba74c965bc0
Diogo Nogueira Lima <diogo.ngr.lima@gmail.com>
2025-01-20 16:35:02 -0300

    add OpenAI integration for generating ads

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common'
-import { IdentityModule } from '@core/identity/identity.module'
-import { PrismaModule } from './services/prisma/prisma.module'
-import { FirebaseModule } from './services/firebase/firebase.module'
+import { Module } from '@nestjs/common';
+import { IdentityModule } from '@core/identity/identity.module';
+import { PrismaModule } from './services/prisma/prisma.module';
+import { FirebaseModule } from './services/firebase/firebase.module';
+import { OpenaiModule } from './services/openai/openai.module'; // Importa o OpenaiModule
 
 @Module({
-	imports: [IdentityModule, PrismaModule, FirebaseModule],
-	controllers: [],
-	providers: []
+  imports: [IdentityModule, PrismaModule, FirebaseModule, OpenaiModule], // Adiciona OpenaiModule
+  controllers: [],
+  providers: []
 })
 export class AppModule {}
