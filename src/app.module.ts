652d9a04e51a226efb51bf755bc9144ca9c578e7
brunom764 <brunom764@gmail.com>
2025-01-27 08:54:45 -0300

    refactor: ad-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { IdentityModule } from '@core/identity/identity.module';
 import { PrismaModule } from './services/prisma/prisma.module';
 import { FirebaseModule } from './services/firebase/firebase.module';
-import { OpenaiModule } from './services/openai/openai.module'; // Importa o OpenaiModule
+import { OpenaiModule } from './services/openai/openai.module';
+import { AdModule } from '@core/ad/ad.module';
 
 @Module({
-  imports: [IdentityModule, PrismaModule, FirebaseModule, OpenaiModule], // Adiciona OpenaiModule
+  imports: [IdentityModule, AdModule, PrismaModule, FirebaseModule, OpenaiModule],
   controllers: [],
   providers: []
 })
--- src/core/ad/ad.module.ts
+++ src/core/ad/ad.module.ts
@@ -3,11 +3,12 @@ import { AdController } from './ad.controller'
 import { AdRepository } from './ad.repository'
 import { AdService } from './ad.service'
 import { OpenaiModule } from 'src/services/openai/openai.module'
+import { IdentityRepository } from '@core/identity/identity.repository'
 
 @Module({
 	imports: [ OpenaiModule ],
 	controllers: [AdController],
-	providers: [AdService, AdRepository],
+	providers: [AdService, AdRepository, IdentityRepository],
 	exports: [AdService]
 })
 export class AdModule {}
