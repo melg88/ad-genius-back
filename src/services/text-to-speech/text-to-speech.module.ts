279136a0b41539d69434f69076ac113cd9fd11ee
Maria Eduarda Lima <meduardalima88@gmail.com>
2025-04-05 01:37:06 -0300

    Delete src/services/text-to-speech/text-to-speech.module.ts

--- src/services/text-to-speech/text-to-speech.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common'
-import { TextToSpeechService } from './text-to-speech.service'
-
-@Global()
-@Module({
-	providers: [TextToSpeechService],
-	exports: [TextToSpeechService]
-})
-export class TextToSpeechModule {}
