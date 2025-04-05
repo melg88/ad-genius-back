66c34c8a47e6f842d74ff97d405f30c2f239b7d8
Maria Eduarda Lima <meduardalima88@gmail.com>
2025-04-05 01:31:08 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,9 @@ import { ValidationPipe } from '@nestjs/common'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import * as express from 'express'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	app.use(express.json({ limit: '50mb' }))
-	app.use(express.urlencoded({ limit: '50mb', extended: false }))
 	app.useGlobalPipes(
 		new ValidationPipe({
 			transform: true
